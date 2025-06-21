4d0efa580abe85a7a0f6ca763c1704ad4d009bb5
Julián Orrego Martínez <jorrego@unal.edu.co>
2025-06-21 14:41:08 -0500

    a

--- Mortydex/app/static/js/mortys.js
+++ Mortydex/app/static/js/mortys.js
@@ -1,19 +1,17 @@
-
-// mortys.js - Lógica con iconos de tipo y texto desplazado
-
 // mortys.js - Lógica con iconos de tipo y texto desplazado
 
 const mortys = [
-  { id: 1, name: 'Morty', type: 'Piedra', img: '/static/images/mortys/pm-001.jpg' },
-  { id: 2, name: 'Test X72', type: 'Papel', img: '/static/images/mortys/pm-008.jpg' },
-  { id: 3, name: 'Morty Conejo', type: 'Papel', img: '/static/images/mortys/pm-015.jpg' },
+  { id: 1, name: 'Morty Cascado', type: 'Piedra', img: '/static/images/mortys/pm-001.jpg' },
+  { id: 2, name: 'Morty Conejo', type: 'Papel', img: '/static/images/mortys/pm-008.jpg' },
+  { id: 3, name: 'Morty Mutado', type: 'Papel', img: '/static/images/mortys/pm-015.jpg' },
   { id: 4, name: 'Morty Fusionado', type: 'Tijera', img: '/static/images/mortys/pm-047.jpg' },
-  { id: 5, name: 'Morty Gato', type: 'Tijera', img: '/static/images/mortys/pm-052.jpg' },
-  { id: 6, name: 'Morty Herido', type: 'Papel', img: '/static/images/mortys/pm-054.jpg' },
-  { id: 7, name: 'Morty Vampiro', type: 'Piedra', img: '/static/images/mortys/pm-058.jpg' },
-  { id: 8, name: 'Morty Rey', type: 'Tijera', img: '/static/images/mortys/pm-059.jpg' },
-  { id: 9, name: 'Morty Ninja', type: 'Papel', img: '/static/images/mortys/pm-085.jpg' },
-  { id: 10, name: 'Morty Afro', type: 'Piedra', img: '/static/images/mortys/pm-087.png' }
+  { id: 5, name: 'Morty Gatero', type: 'Tijera', img: '/static/images/mortys/pm-052.jpg' },
+  { id: 6, name: 'Morty Gymbro', type: 'Papel', img: '/static/images/mortys/pm-054.jpg' },
+  { id: 7, name: 'Morty Venezolano', type: 'Piedra', img: '/static/images/mortys/pm-058.jpg' },
+  { id: 8, name: 'Morty Mago', type: 'Tijera', img: '/static/images/mortys/pm-059.jpg' },
+  { id: 9, name: 'Morty Mullet', type: 'Papel', img: '/static/images/mortys/pm-085.jpg' },
+  { id: 10, name: 'Morty Afro', type: 'Piedra', img: '/static/images/mortys/pm-087.png' },
+  { id: 11, name: 'Morty Afro', type: 'Piedra', img: '/static/images/mortys/pm-087.png' }
 ];
 
 let currentPage = 1;
